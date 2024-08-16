668b730b46c6748b0adf95699f58f3648cc02b79
emmetl913 <elars1002@gmail.com>
2024-08-16 14:39:48 -0400

    startin games

--- games/ChoppingGame.gd
+++ games/ChoppingGame.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-@export var first_game_index : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func _input(event):
+	if event.is_action_pressed("LMB"):
+		pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
