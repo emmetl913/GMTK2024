2735983c9f645b628025884da9687c54275115f0
emmetl913 <elars1002@gmail.com>
2024-08-16 14:07:05 -0400

    MM start button

--- menus/MainMenu.gd
+++ menus/MainMenu.gd
@@ -8,8 +8,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_tree().change_scene_to_file("res://worlds/MicroGamePlayer.tscn")
+	pass
 
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://worlds/MicroGamePlayer.tscn")
