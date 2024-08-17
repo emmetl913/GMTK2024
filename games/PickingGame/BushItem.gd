8b0fbbe1b7e25285562e3e60d24b295cc2a0b022
bwoodward523 <bwoodward@ycp.edu>
2024-08-17 12:03:00 -0400

    wip

--- games/PickingGame/BushItem.gd
+++ games/PickingGame/BushItem.gd
@@ -1,2 +1,12 @@
 extends Node2D
 
+var in_range = false
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("Cursor"):
+		in_range = true
+
+
+func _on_area_2d_area_exited(area):
+	if area.is_in_group("Cursor"):
+		in_range = false
--- games/PickingGame/PickingGame.gd
+++ games/PickingGame/PickingGame.gd
@@ -1,12 +1,19 @@
 extends Base_Game
 
-
+var bushItems: Array[Node2D]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Setup timers
 	timers = [5, 4, 3, 2]
-	
-	pass # Replace with function body.
+	for i in get_children():
+		if i.is_in_group("BushItem"):
+			bushItems.append(i)
+
+func _input(event):
+	if event.is_action_pressed("LMB"):
+		for i in bushItems:
+			if i.in_range:
+				i.self_modulate = Color.GREEN
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
