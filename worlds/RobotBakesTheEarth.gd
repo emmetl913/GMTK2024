37f16585339bd6e8ed50d269076b14601983ab5c
bwoodward523 <bwoodward@ycp.edu>
2024-08-19 16:33:53 -0400

    Shooting star and animation fix

--- worlds/RobotBakesTheEarth.gd
+++ worlds/RobotBakesTheEarth.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var dont_repeat = false
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
@@ -9,13 +10,20 @@ func _ready():
 	$AnimatedSprite2D.play("Booom")
 
 func _on_animated_sprite_2d_animation_finished():
-	$AnimationPlayer.play("ShowYouWin")
-	$YouWinText.visible = true
+	if !dont_repeat: 
+		$AnimationPlayer.play("ShowYouWin")
+		$YouWinText.visible = true
+	$PlayShootingStar.start(randf_range(0,10))
 
 func _on_animation_player_animation_finished(anim_name):
 	if (anim_name == "ShowYouWin"):
 		$AnimationPlayer.play("ShowMainMenuButton")
 		$MainMenuButton.visible = true
-
+		dont_repeat = true
+		
 func _on_main_menu_button_pressed():
 	get_tree().change_scene_to_file("res://menus/MainMenu.tscn")
+
+
+func _on_play_shooting_star_timeout():
+	$AnimatedSprite2D.play("ShootingStar")
